6970b617547e67b5d43f48a9b15a62fed4267557
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-24 11:08:00 -0700

    Initial Commit

---
+++ UIKitPro6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPro6
+//
+//  Created by Sothesom on 03/05/1403.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
