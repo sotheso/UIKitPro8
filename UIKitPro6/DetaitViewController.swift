b40877b734c64ddc39e4f2d374c49351e1349f4e
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-24 14:27:13 -0700

    Rendering a petition: loadHTMLString

---
+++ UIKitPro6/DetaitViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetaitViewController.swift
+//  UIKitPro6
+//
+//  Created by Sothesom on 03/05/1403.
+//
+
+import UIKit
+import WebKit
+
+class DetaitViewController: UIViewController {
+    // خاصیت وب
+    var webView : WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // بررسی اینکه واقعا جزئیات وجود دارد
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UIKitPro6/ViewController.swift
+++ UIKitPro6/ViewController.swift
@@ -46,5 +46,11 @@ class ViewController: UITableViewController {
         cell.detailTextLabel?.text = petition.body
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetaitViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true )
+    }
 }
 
