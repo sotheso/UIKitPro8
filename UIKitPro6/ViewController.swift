64391af3d7fc3eac515534295822fe951702a63c
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-24 11:28:12 -0700

    Start Project

--- UIKitPro6/ViewController.swift
+++ UIKitPro6/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+         // ایجاد برچسب متن سلول و کپشن
+        cell.textLabel?.text = "Title"
+        cell.detailTextLabel?.text = "Suntitle"
+        return cell
+    }
 }
 
